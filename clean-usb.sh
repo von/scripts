455a86a51780e357909e089b3fe1b0e0ce98f4b1
Von Welch <von@vwelch.com>
2022-12-27 11:49:36 -0500

    clean-usb.sh: added directions regarding full disk access

--- clean-usb.sh
+++ clean-usb.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # Clean all the stuff MacOSX puts on a USB stick
+#
+# Note that whatever terminal program this is run in will need
+# full disk access permissions or you will get "Operation not
+# permitted" errors.
+# System Preferences ->  Security and Privacy -> Privacy -> Full Disk Access
 if test $# -ne 1 ; then
   echo "$0 <path to USB>"
   exit 1
@@ -15,6 +20,9 @@ fi
 set errexit
 
 echo "Cleaning \"${usb_path}\""
+echo "If you get errors, make sure the terminal program has"
+echo "full disk access permissions:"
+echo "  System Preferences ->  Security and Privacy -> Privacy -> Full Disk Access"0
 cd "${usb_path}"
 rm -rf .Spotlight-V100 .fseventsd
 find . -type f -name '\._*' -delete
