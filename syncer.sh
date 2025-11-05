fef26406e4b40eaceaa5b22c46625e4d4c8396f8
Von Welch <von@vwelch.com>
2025-11-05 12:21:32 -0500

    syncer.sh: added do_sync()

--- syncer.sh
+++ syncer.sh
@@ -35,6 +35,26 @@ do_rsync()
   caffeinate -ims rsync -urv --size-only --delete-during --ignore-errors . "${dst}"
 }
 
+# do_sync <path1> <path2>
+#  Make <path1> and <path2>, coping any files not in one to the other
+do_sync()
+{
+  path1=$1; shift
+  path2=$1; shift
+  test -n "$dst" || { echo "Usage: do_sync <path1> <path2>" ; return 1 ; }
+
+  cd "${path1}"
+  # rsync options:
+  # --size-only: I don't think the NAS keeps accurate timestamps
+  # --ignore-errors: .Spotlight-V100 can cause errors
+  #
+  # caffeinate options:
+  #  -i: prevent system from idle sleeping
+  #  -m: prevent disk from idle sleeping
+  #  -s: prevent system from sleeping on AC power
+  caffeinate -ims rsync -urv --size-only --ignore-errors . "${path2}"
+}
+
 # Leading colon means silent errors, script will handle them
 # Colon after a parameter, means that parameter has an argument in $OPTARG
 while getopts ":h" opt; do
